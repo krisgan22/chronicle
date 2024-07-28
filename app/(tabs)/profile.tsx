8c567238fbe5f7c01e441b0897e2bf22167241e3
Ganeshram Krishnamoorthy <g.krishnamoorthy1@wsu.edu>
2024-07-28 12:13:35 -0700

    updated profile screen to display user details and new sign-out button, currently user details are not editable

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { useAppwriteContext } from '@/appwrite_backend/AppwriteContext';
 import { signOutAccountDeleteSession, getOrganizations } from '@/appwrite_backend/service';
@@ -6,9 +6,10 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CustomButton from '@/components/CustomButton';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const Profile = () => {
-    const { user, setUser, setIsSignedIn} = useAppwriteContext();
+    const { user, setUser, userDetails, setUserDetails, setIsSignedIn} = useAppwriteContext();
     const [isSubmitting, setIsSubmitting] = useState(false)
 
 
@@ -19,7 +20,11 @@ const Profile = () => {
             
             // Remove from Async Storage
             await AsyncStorage.removeItem('user');
-            console.log("Async Storage remove user");
+            console.log("Async Storage remove user session");
+
+            await AsyncStorage.removeItem('userDetails');
+            console.log("Async Storage remove user details");
+
             await AsyncStorage.removeItem('welcomeMsg');
             console.log("Async Storage remove welcomeMsg flag");
 
@@ -38,13 +43,46 @@ const Profile = () => {
 
   return (
     <SafeAreaView className='h-full m-5'>
-        <CustomButton
-            title='Log Out'
-            handlePress={submit}
-            containerStyles='bg-rose-700'
-            isLoading={isSubmitting}
-            textStyles='text-base font-medium text-white'
-        />
+        <ScrollView className='flex'>
+            <View className='flex flex-row justify-between'>
+                <Text className="text-2xl font-bold">
+                    {userDetails.username}
+                </Text>
+                <TouchableOpacity 
+                    onPress={submit}
+                    className='pt-1'
+                    >
+                    <MaterialIcons name="logout" size={24} color="#BE123C" />
+                </TouchableOpacity>
+            </View>
+            <View>
+                <Text className='font-semibold text-lg mt-5'>First Name</Text>
+                <Text className='font-normal text-lg'>{userDetails.first_name}</Text>
+                
+                <Text className='font-semibold text-lg mt-5'>Last Name</Text>
+                <Text className='font-normal text-lg'>{userDetails.last_name}</Text>
+                
+                <Text className='font-semibold text-lg mt-5'>Phone Number</Text>
+                <Text className='font-normal text-lg'>{userDetails.phone_num}</Text>
+                
+                <Text className='font-semibold text-lg mt-5'>Email</Text>
+                <Text className='font-normal text-lg'>{userDetails.email}</Text>
+                
+                <Text className='font-semibold text-lg mt-5'>Employer</Text>
+                <Text className='font-normal text-lg'>{userDetails.employer}</Text>
+                
+                <Text className='font-semibold text-lg mt-5'>Matching Rate</Text>
+                <Text className='font-normal text-lg'>${userDetails.matching_rate}/hr</Text>
+            </View>
+
+            {/* <CustomButton
+                title='Log Out'
+                handlePress={submit}
+                containerStyles='bg-rose-700'
+                isLoading={isSubmitting}
+                textStyles='text-base font-medium text-white'
+            /> */}
+        </ScrollView>
     </SafeAreaView>
   )
 }
