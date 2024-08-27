e26b658bdcac57084c1138bc030bdec453bd2558
Ganeshram Krishnamoorthy <g.krishnamoorthy1@wsu.edu>
2024-08-26 17:22:33 -0700

    added inter fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,55 @@
 import AppwriteProvider from "@/appwrite_backend/AppwriteContext";
 import { StatusBar } from "expo-status-bar";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Inter-Black": require("../assets/fonts/Inter-Black.ttf"),
+    "Inter-BlackItalic": require("../assets/fonts/Inter-BlackItalic.ttf"),
+    "Inter-Bold": require("../assets/fonts/Inter-Bold.ttf"),
+    "Inter-BoldItalic": require("../assets/fonts/Inter-BoldItalic.ttf"),
+    "Inter-ExtraBold": require("../assets/fonts/Inter-ExtraBold.ttf"),
+    "Inter-ExtraBoldItalic": require("../assets/fonts/Inter-ExtraBoldItalic.ttf"),
+    "Inter-ExtraLight": require("../assets/fonts/Inter-ExtraLight.ttf"),
+    "Inter-ExtraLightItalic": require("../assets/fonts/Inter-ExtraLightItalic.ttf"),
+    "Inter-Italic": require("../assets/fonts/Inter-Italic.ttf"),
+    "Inter-Light": require("../assets/fonts/Inter-Light.ttf"),
+    "Inter-LightItalic": require("../assets/fonts/Inter-LightItalic.ttf"),
+    "Inter-Medium": require("../assets/fonts/Inter-Medium.ttf"),
+    "Inter-MediumItalic": require("../assets/fonts/Inter-MediumItalic.ttf"),
+    "Inter-Regular": require("../assets/fonts/Inter-Regular.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/Inter-SemiBold.ttf"),
+    "Inter-SemiBoldItalic": require("../assets/fonts/Inter-SemiBoldItalic.ttf"),
+    "Inter-Thin": require("../assets/fonts/Inter-Thin.ttf"),
+    "Inter-ThinItalic": require("../assets/fonts/Inter-ThinItalic.ttf"),
+    "InterDisplay-Black": require("../assets/fonts/InterDisplay-Black.ttf"),
+    "InterDisplay-BlackItalic": require("../assets/fonts/InterDisplay-BlackItalic.ttf"),
+    "InterDisplay-Bold": require("../assets/fonts/InterDisplay-Bold.ttf"),
+    "InterDisplay-BoldItalic": require("../assets/fonts/InterDisplay-BoldItalic.ttf"),
+    "InterDisplay-ExtraBold": require("../assets/fonts/InterDisplay-ExtraBold.ttf"),
+    "InterDisplay-ExtraBoldItalic": require("../assets/fonts/InterDisplay-ExtraBoldItalic.ttf"),
+    "InterDisplay-ExtraLight": require("../assets/fonts/InterDisplay-ExtraLight.ttf"),
+    "InterDisplay-ExtraLightItalic": require("../assets/fonts/InterDisplay-ExtraLightItalic.ttf"),
+    "InterDisplay-Italic": require("../assets/fonts/InterDisplay-Italic.ttf"),
+    "InterDisplay-Light": require("../assets/fonts/InterDisplay-Light.ttf"),
+    "InterDisplay-LightItalic": require("../assets/fonts/InterDisplay-LightItalic.ttf"),
+    "InterDisplay-Medium": require("../assets/fonts/InterDisplay-Medium.ttf")
+  })
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+
+  }, [fontsLoaded, error])
+  
+  if (!fontsLoaded && !error) return null;
+
   return (
     <GestureHandlerRootView>
     <BottomSheetModalProvider>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,43 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        interBlack: ["Inter-Black", "sans-serif"],
+        interBlackItalic: ["Inter-BlackItalic", "sans-serif"],
+        interBold: ["Inter-Bold", "sans-serif"],
+        interBoldItalic: ["Inter-BoldItalic", "sans-serif"],
+        interExtraBold: ["Inter-ExtraBold", "sans-serif"],
+        interExtraBoldItalic: ["Inter-ExtraBoldItalic", "sans-serif"],
+        interExtraLight: ["Inter-ExtraLight", "sans-serif"],
+        interExtraLightItalic: ["Inter-ExtraLightItalic", "sans-serif"],
+        interItalic: ["Inter-Italic", "sans-serif"],
+        interLight: ["Inter-Light", "sans-serif"],
+        interLightItalic: ["Inter-LightItalic", "sans-serif"],
+        interMedium: ["Inter-Medium", "sans-serif"],
+        interMediumItalic: ["Inter-MediumItalic", "sans-serif"],
+        interRegular: ["Inter-Regular", "sans-serif"],
+        interSemiBold: ["Inter-SemiBold", "sans-serif"],
+        interSemiBoldItalic: ["Inter-SemiBoldItalic", "sans-serif"],
+        interThin: ["Inter-Thin", "sans-serif"],
+        interThinItalic: ["Inter-ThinItalic", "sans-serif"],
+        interDisplayBlack: ["InterDisplay-Black", "sans-serif"],
+        interDisplayBlackItalic: ["InterDisplay-BlackItalic", "sans-serif"],
+        interDisplayBold: ["InterDisplay-Bold", "sans-serif"],
+        interDisplayBoldItalic: ["InterDisplay-BoldItalic", "sans-serif"],
+        interDisplayExtraBold: ["InterDisplay-ExtraBold", "sans-serif"],
+        interDisplayExtraBoldItalic: ["InterDisplay-ExtraBoldItalic", "sans-serif"],
+        interDisplayExtraLight: ["InterDisplay-ExtraLight", "sans-serif"],
+        interDisplayExtraLightItalic: ["InterDisplay-ExtraLightItalic", "sans-serif"],
+        interDisplayItalic: ["InterDisplay-Italic", "sans-serif"],
+        interDisplayLight: ["InterDisplay-Light", "sans-serif"],
+        interDisplayLightItalic: ["InterDisplay-LightItalic", "sans-serif"],
+        interDisplayMedium: ["InterDisplay-Medium", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
