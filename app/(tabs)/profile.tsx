308b9815ebdc2f610e37ea4f002d1a7cb9bc39ca
Ganeshram Krishnamoorthy <g.krishnamoorthy1@wsu.edu>
2024-08-29 15:19:16 -0700

    created confirm modal component and added it to profile screen

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Modal, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import { useAppwriteContext } from '@/appwrite_backend/AppwriteContext';
 import { signOutAccountDeleteSession, getOrganizations } from '@/appwrite_backend/service';
@@ -7,6 +7,7 @@ import { router } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import CustomButton from '@/components/CustomButton';
 import { MaterialIcons } from '@expo/vector-icons';
+import ConfirmModal from '@/components/ConfirmModal';
 
 const Profile = () => {
     const { user, setUser, userDetails, setUserDetails, setIsSignedIn} = useAppwriteContext();
@@ -42,63 +43,72 @@ const Profile = () => {
         console.log("user: ", user);
     }
 
-  return (
-    <SafeAreaView className='h-full m-5'>
-        <ScrollView className='flex'>
-            <View className='flex flex-row justify-between'>
-                <Text className="text-2xl font-bold">
-                    {userDetails.username}
-                </Text>
-                <TouchableOpacity 
-                    onPress={submit}
-                    className='pt-1'
-                    >
-                    <MaterialIcons name="logout" size={24} color="#BE123C" />
-                </TouchableOpacity>
-            </View>
-            <View>
-                <Text className='font-semibold text-lg mt-5'>First Name</Text>
-                <Text className='font-normal text-lg'>{userDetails.first_name}</Text>
-                
-                <Text className='font-semibold text-lg mt-5'>Last Name</Text>
-                <Text className='font-normal text-lg'>{userDetails.last_name}</Text>
-                
-                <Text className='font-semibold text-lg mt-5'>Phone Number</Text>
-                <Text className='font-normal text-lg'>{userDetails.phone_num}</Text>
-                
-                <Text className='font-semibold text-lg mt-5'>Email</Text>
-                <Text className='font-normal text-lg'>{userDetails.email}</Text>
+    const [confirmModalVisible, setConfirmModalVisible] = useState(false);
 
-                <Text className='font-semibold text-lg mt-5'>Employer</Text>
-                {userDetails.employer ? 
-                <>
-                    <Text className='font-normal text-lg'>{userDetails.employer}</Text>
-                </> : 
-                <>
-                    <Text className='font-normal text-lg'>-</Text>
-                </>}
+    return (
+        <SafeAreaView className='h-full m-5'>
+            <ConfirmModal
+                confirmModalVisible={confirmModalVisible}
+                setConfirmModalVisible={setConfirmModalVisible}
+                modalText='Are you sure you want to Log Out?'
+                handleSubmit={submit}
+            >
+            </ConfirmModal>
+            <ScrollView className='flex'>
+                <View className='flex flex-row justify-between'>
+                    <Text className="text-2xl font-bold">
+                        {userDetails.username}
+                    </Text>
+                    <TouchableOpacity 
+                        onPress={() => setConfirmModalVisible(true)}
+                        className='pt-1'
+                        >
+                        <MaterialIcons name="logout" size={24} color="#BE123C" />
+                    </TouchableOpacity>
+                </View>
+                <View>
+                    <Text className='font-semibold text-lg mt-5'>First Name</Text>
+                    <Text className='font-normal text-lg'>{userDetails.first_name}</Text>
+                    
+                    <Text className='font-semibold text-lg mt-5'>Last Name</Text>
+                    <Text className='font-normal text-lg'>{userDetails.last_name}</Text>
+                    
+                    <Text className='font-semibold text-lg mt-5'>Phone Number</Text>
+                    <Text className='font-normal text-lg'>{userDetails.phone_num}</Text>
+                    
+                    <Text className='font-semibold text-lg mt-5'>Email</Text>
+                    <Text className='font-normal text-lg'>{userDetails.email}</Text>
 
-                <Text className='font-semibold text-lg mt-5'>Matching Rate</Text>
-                {userDetails.matching_rate ? 
-                <>
-                    <Text className='font-normal text-lg'>${userDetails.matching_rate}/hr</Text>
-                </> : 
-                <>
-                    <Text className='font-normal text-lg'>-</Text>
-                </>}
+                    <Text className='font-semibold text-lg mt-5'>Employer</Text>
+                    {userDetails.employer ? 
+                    <>
+                        <Text className='font-normal text-lg'>{userDetails.employer}</Text>
+                    </> : 
+                    <>
+                        <Text className='font-normal text-lg'>-</Text>
+                    </>}
 
-            </View>
+                    <Text className='font-semibold text-lg mt-5'>Matching Rate</Text>
+                    {userDetails.matching_rate ? 
+                    <>
+                        <Text className='font-normal text-lg'>${userDetails.matching_rate}/hr</Text>
+                    </> : 
+                    <>
+                        <Text className='font-normal text-lg'>-</Text>
+                    </>}
 
-            {/* <CustomButton
-                title='Log Out'
-                handlePress={submit}
-                containerStyles='bg-rose-700'
-                isLoading={isSubmitting}
-                textStyles='text-base font-medium text-white'
-            /> */}
-        </ScrollView>
-    </SafeAreaView>
-  )
+                </View>
+
+                {/* <CustomButton
+                    title='Log Out'
+                    handlePress={submit}
+                    containerStyles='bg-rose-700'
+                    isLoading={isSubmitting}
+                    textStyles='text-base font-medium text-white'
+                /> */}
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
 
 export default Profile
---
+++ components/ConfirmModal.tsx
@@ -0,0 +1,71 @@
+import { View, Text, Modal, StyleSheet } from 'react-native'
+import React from 'react'
+import CustomButton from './CustomButton'
+
+type ConfirmModalProps = {
+    confirmModalVisible: boolean,
+    setConfirmModalVisible: ((item: boolean) => void),
+    handleSubmit: ((...args: any[]) => any),
+    modalText: string,
+}
+
+const ConfirmModal = ({confirmModalVisible, setConfirmModalVisible, handleSubmit, modalText}: ConfirmModalProps) => {
+  return (
+    <Modal
+        animationType="fade"
+        transparent={true}
+        visible={confirmModalVisible}
+        onRequestClose={() => {
+            setConfirmModalVisible(!confirmModalVisible);
+        }}>
+        <View style={styles.modalBackground}>
+            <View style={styles.modalView}>
+                <Text className='text-base'>{modalText}</Text>
+                <View className='flex-row'>
+                    <CustomButton
+                        handlePress={() => setConfirmModalVisible(!confirmModalVisible)}
+                        title='Cancel'
+                        textStyles='text-base text-red-600 mx-5'
+                        >
+                    </CustomButton>
+                    <CustomButton
+                        handlePress={handleSubmit}
+                        title='Confirm'
+                        textStyles='text-base text-green-600 mx-5'
+                        >
+                    </CustomButton>
+                </View>
+            </View>
+        </View>
+    </Modal>
+  )
+}
+
+const styles = StyleSheet.create({
+    modalView: {
+      margin: 20,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      paddingLeft: 30,
+      paddingRight: 30,
+      paddingTop: 30,
+      paddingBottom: 10,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    modalBackground: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',  // Dark overlay with 50% opacity
+    },
+});
+
+export default ConfirmModal
